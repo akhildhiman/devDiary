52e12d0dd9a79b61c4d2408d6dfd809b29259872
akhil dhiman <dhimanakhil95@gmail.com>
2019-08-17 19:45:12 +0530

    update

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,16 +20,16 @@ const AboutPage = (props) => {
                     <SEO title="About" />
                     <div className="mt-3">
                         <h2 className="heading">About</h2>
-                        <p><i>Developer Diary is a Gatsby Starter blog template created with web developers in mind, but really, anyone can use it. It's totally usable right out of the box, but minimalist enough to be easily modifiable to suit your needs.</i></p>
+                        <p><i>Hola, Everyone! I'm Akhil, I write mostly about JavaScript and other related stuff. Currently in the process of becoming a Full- Stack Web Developer, I'm also an old school gamer, sports enthusiast, and a really really bad Ukulele player. My primary purpose of writing here is to explain complex technical stuff in the simplest possible way.</i></p>
                         <br />
-                        <h4>Features</h4>
-                        <div>
+                        {/* <h4>Features</h4> */}
+                        {/* <div>
                             <span className="text-success d-inline-block" title="blazing">
                                 <FaCheckCircle size={26} style={{ color: "success" }} />
                             </span>
                             <p className="d-inline-block ml-3 w-75 align-top">Blazing fast, as you'd expect from a Gatsby site</p>
-                        </div>
-                        <div>
+                        </div> */}
+                        {/* <div>
                             <span className="text-success d-inline-block" title="tags">
                                 <FaCheckCircle size={26} style={{ color: "success" }} />
                             </span>
@@ -40,8 +40,8 @@ const AboutPage = (props) => {
                                 <TechTag tag="html" tech="HTML" name="FaHtml5" size={20} color="darkorange" />
                                 <TechTag tag="css" tech="CSS" name="DiCss3Full" size={20} color="teal" />
                             </div>  
-                        </div>
-                        <div className="mt-4">
+                        </div> */}
+                        {/* <div className="mt-4">
                             <span className="text-success d-inline-block" title="prism">
                                 <FaCheckCircle size={26} style={{ color: "success" }} />
                             </span>
@@ -52,13 +52,13 @@ const AboutPage = (props) => {
                                 <FaCheckCircle size={26} style={{ color: "success" }} />
                             </span>
                             <p className="d-inline-block ml-3 w-75 align-top">Developer-relevant social-media icon links including GitHub, Stack Overflow and freeCodeCamp</p>
-                        </div>
-                        <div>
+                        </div> */}
+                        {/* <div>
                             <span className="text-success d-inline-block" title="mobile">
                                 <FaCheckCircle size={26} style={{ color: "success" }} />
                             </span>
                             <p className="d-inline-block ml-3 w-75 align-top">Mobile responsive, of course</p>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
